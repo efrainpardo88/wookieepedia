45e766180148976bdcc472a42c157ea479d4acf8
efrain.pardo <efrain.pardo@globant.com>
2020-04-13 14:52:13 -0500

    #5
    * Create layout components

--- src/Wookieepedia.UI/src/app/app.module.ts
+++ src/Wookieepedia.UI/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewLayoutComponent } from './shared/layout/view-layout/view-layout.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { MainContentComponent } from './shared/layout/main-content/main-content.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewLayoutComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
