4a811b28bd6fe0edf0b4b042558e1a9215b264f1
efrain.pardo <efrain.pardo@globant.com>
2020-04-21 22:54:28 -0500

    Fix PR comments

--- src/Wookieepedia.UI/src/app/home/home.component.ts
+++ src/Wookieepedia.UI/src/app/home/home.component.ts
@@ -10,23 +10,6 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  galaxi = [
-    {
-      id: '1',
-      image: 'assets/images/InGalaxi.png',
-      title: 'In Galaxy',
-      button: 'Show more'
-    },
-  ];
-  Wook = [
-    {
-      id: '2',
-      image: 'assets/images/Wook.png',
-      title: 'Wook',
-      button: 'Show more'
-    },
-  ];
-
   faChevronDown = faChevronDown;
 
   ngOnInit(): void {
