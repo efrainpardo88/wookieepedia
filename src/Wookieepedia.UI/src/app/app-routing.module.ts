3677ec596f6f6793715b1a2d4c9901c81c10eb3e
efrain.pardo <efrain.pardo@globant.com>
2020-04-28 14:38:56 -0500

    Fix merge from brian

--- src/Wookieepedia.UI/src/app/app-routing.module.ts
+++ src/Wookieepedia.UI/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
-import {ResoursesComponent} from './resourses/resourses.component'
-import { AppComponent } from './app.component';
 import { ViewLayoutComponent } from './shared/layout/view-layout/view-layout.component';
 
-import {SearchComponent} from './search/search.component';
+import { HomeComponent } from './home/home.component';
+
+import { SearchComponent } from './search/search.component';
+
+import { ResoursesComponent } from './resourses/resourses.component'
 import { PeopleComponent } from './resourses/people/people.component';
 import { PlanetsComponent } from './resourses/planets/planets.component';
 import { FilmsComponent } from './resourses/films/films.component';
@@ -17,13 +17,12 @@ import { StarshipsComponent } from './resourses/starships/starships.component';
 
 const routes: Routes = [
   {
-     path: '', redirectTo: 'home', pathMatch: 'prefix' 
+    path: '', redirectTo: 'home', pathMatch: 'prefix'
   },
-  
   {
     path: '',
     component: ViewLayoutComponent,
-    
+
     children: [{
       path: 'home',
       component: HomeComponent
@@ -33,31 +32,31 @@ const routes: Routes = [
       component: ResoursesComponent,
       children: [
         {
-        path: 'people',
-        component: PeopleComponent
+          path: 'people',
+          component: PeopleComponent
         },
         {
-        path: 'planets',
-        component: PlanetsComponent
+          path: 'planets',
+          component: PlanetsComponent
         },
         {
-        path: 'films',
-        component: FilmsComponent
+          path: 'films',
+          component: FilmsComponent
         },
         {
-        path: 'species',
-        component: SpeciesComponent
+          path: 'species',
+          component: SpeciesComponent
         },
         {
-        path: 'vehicles',
-        component: VehiclesComponent
+          path: 'vehicles',
+          component: VehiclesComponent
         },
         {
-        path: 'starships',
-        component: StarshipsComponent
+          path: 'starships',
+          component: StarshipsComponent
         },
-      ]  
-      
+      ]
+
     },
     {
       path: 'search',
