0a4ccecee62d496b8d6dd8d8abb4e41bad365d39
jpgm1217 <juanpa1217@hotmail.es>
2020-04-30 15:16:26 -0500

    Models and Images
    se agregaron los modelos y imagenes faltantes

---
+++ src/Wookieepedia.UI/src/app/models/movies.ts
@@ -0,0 +1,15 @@
+import { Card } from './card';
+
+export interface Movies {
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: Date;
+    species: Card[];
+    starships: Card[];
+    vehicles: Card[];
+    characters: Card[];
+    planets: Card[]; 
+}
---
+++ src/Wookieepedia.UI/src/app/models/vehicles.ts
@@ -0,0 +1,17 @@
+import { Card } from './card';
+
+export interface Vehicles {
+  name: string;
+  model: string;
+  vehicle_class: string;
+  manufacturer: string;
+  length: string;
+  cost_in_credits: string;
+  crew: string;
+  passengers: string;
+  max_atmosphering_speed: string;
+  cargo_capacity: string;
+  consumables: string;
+  films: Card[];
+  pilots: Card[];  
+}
