2b56b0076d8d9fbda21498080dfb025c14a7c373
jpgm1217 <juanpa1217@hotmail.es>
2020-05-03 22:19:40 -0500

    resources Starships

--- src/Wookieepedia.UI/src/app/resources/starships/starships.component.ts
+++ src/Wookieepedia.UI/src/app/resources/starships/starships.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { StarshipsService } from 'src/app/services/starships.service';
+import { CardsService } from 'src/app/services/cards.service';
+import { Starship } from '../../models/starship';
 @Component({
   selector: 'app-starships',
   templateUrl: './starships.component.html',
   styleUrls: ['./starships.component.css']
 })
 export class StarshipsComponent implements OnInit {
+  resource: Starship;
+  resourceImage: string;
+  loading = false;
 
-  constructor() { }
-
+  constructor(
+    private route: ActivatedRoute, 
+    private titleService: Title,
+    private service: StarshipsService,
+    private cards: CardsService
+    ) { }
+    
   ngOnInit(): void {
+    this.loading = true;
+    this.route.params.subscribe(params => {
+      this.service.getStarshipById(params.id).subscribe(data => {
+        this.titleService.setTitle(data.title + ' | Wookieepedia');
+        this.resource = this.cards.getResourceWithCards(data);
+        this.loading = false;
+      });
+    });
   }
 
 }
